27878506bcd3eb5637620c0ece84c18844350441
Moccasym <57842536+Moccasym@users.noreply.github.com>
2023-10-06 10:03:57 +0200

    cleaned up code

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -6,7 +6,6 @@ function ExpenseDate(props) {
     const year = props.date.getFullYear();
 
   return (
-   
       <div className='expense-date'>
         <div className='expense-date__month'>{month}</div>
         <div className='expense-date__day'>{day}</div>
