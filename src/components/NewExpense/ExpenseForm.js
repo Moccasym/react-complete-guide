a4ff1c1f4563b6a24421d60093ab6cb77a14b889
Moccasym <57842536+Moccasym@users.noreply.github.com>
2023-10-26 23:47:25 +0200

    adding states for inputs

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,22 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    //Function when an input is given and an Event is triggered with "onChange"
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+    };
+
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+    };
+
     return(
     <form>
         <div className="new-expense__controls">
             <div className="new-expense__control">
-                <label>Titel</label>
-                <input type='text' />
+                <label>{enteredTitle}</label>
+                <input type='text' onChange={titleChangeHandler}/>
             </div>
             <div className="new-expense__control">
-                <label>Amount</label>
-                <input type='number' min='1' steps='1' />
+                <label>{enteredAmount}</label>
+                <input type='number' min='1' steps='1' onChange={titleChangeHandler}/>
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type='date' min='2019-01-01' max='2023-12-31' />
+                <input type='date' min='2019-01-01' max='2023-12-31' onChange={amountChangeHandler}/>
             </div>
         </div>
         <div className="new-expense__actions">
