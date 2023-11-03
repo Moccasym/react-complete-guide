a8d6a4fc34c7b59c63719b99d936acec80f8ae3b
Moccasym <57842536+Moccasym@users.noreply.github.com>
2023-11-03 19:59:20 +0100

    adding filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,19 +2,30 @@ import React from 'react';
 import './Expenses.css';
 import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card';
+import ExpenseFilter from '../ExpenseFilter/ExpenseFilter';
 
 const Expenses = (props) => {
+
+  const onFilterChangeHandler = (selectedYear) => {
+    console.log(selectedYear);
+  };
+
   return (
-    <Card className="expenses">
-      {props.items.map((item, index) => (
-        <ExpenseItem
-          key={item.id}  // Make sure to assign a unique key for each item
-          title={item.title}
-          amount={item.amount}
-          date={item.date}
-        />
-      ))}
-    </Card>
+    <div>
+      <Card>
+      <ExpenseFilter onFilter={onFilterChangeHandler}/>
+      </Card>
+      <Card className="expenses">
+        {props.items.map((item, index) => (
+          <ExpenseItem
+            key={item.id}  // Make sure to assign a unique key for each item
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          />
+        ))}
+      </Card>
+    </div>
   );
 }
 
